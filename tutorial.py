ed4717c43eb60240fa4382311420fb48f227abdf
Eluyscuti <abhikaushikkar@gmail.com>
2022-01-19 10:33:00 -0800

    Update tutorial.py
    can now check for tutorials in students classes

--- tutorial.py
+++ tutorial.py
@@ -1,45 +1,22 @@
 import pandas as pd
-import openpyxl
-import numpy
+from student import Student
 
-class Student:
-    students = []
-    def __init__(self, school, grade, fname, lname):
-        self.school = school
-        self.grade = grade
-        self.fname = fname
-        self.lname = lname
-        self.classes = {}
-
-
-    def add_classes(self, period, teacher):
-        self.classes[period] = teacher
-
-    @classmethod
-    def add_student(cls, student):
-        cls.students.append(student)
-
-student1 = Student("Leland", 9, "Arnav", "Vajirkar")
-Student.add_student(student1)
-student1.add_classes(1, "Sarkar")
-student1.add_classes(2, "Yen")
-
-
-data = "School.csv"
-df = pd.read_csv(data)
-
-df.set_index("Name", inplace=True)
-
-open_teachers = []
 def get_tutorial(student, day):
-    for period in student.classes:
-        if str(df.loc[student.classes[period]].loc[day]) == "open": #line doesnt update after first time so that needs fixing
+    open_teachers = []
+    data = "School.csv" 
+    df = pd.read_csv(data) #reads csv into dataframe
+
+    df.set_index("Name", inplace=True) #Makes name attribute the way to index rows
+
+    """Loops Through every period in classes dictionary.
+    Gets name of each teacher. Indexes that row and column for day passed in.
+    Checks if that slot is open, and if so adds it as an open teacher"""
+    
+    for period in student.classes: 
+        if str(df.loc[student.classes[period]].loc[day]) == "open": #gets teacher and day slot in csv and checks whether open or not
             open_teachers.append(student.classes[period])
 
-
-
-get_tutorial(student1, "Tuesday")
-print(open_teachers)
+    return open_teachers
 
 
 
