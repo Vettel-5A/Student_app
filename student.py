421c7c15d9a0ad0ac53cede8c62519ac6abc1b4e
Eluyscuti <abhikaushikkar@gmail.com>
2022-01-19 10:28:32 -0800

    Update student.py
    Student class created

--- student.py
+++ student.py
@@ -1,4 +1,20 @@
+class Student:
+    students = []
+    def __init__(self, school, grade, fname, lname, gpa):
+        self.school = school
+        self.grade = grade
+        self.fname = fname
+        self.lname = lname
+        self.classes = {}
+        self.gpa = gpa
 
+
+    def add_classes(self, period, teacher):
+        self.classes[period] = teacher
+
+    @classmethod
+    def add_student(cls, student):
+        cls.students.append(student)
 
 
 
