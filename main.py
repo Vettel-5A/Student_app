1b101a71e2b0baae47c3fcaca773dc9884190ca2
Eluyscuti <abhikaushikkar@gmail.com>
2022-01-19 10:27:57 -0800

    Update main.py
    infra for main.py done

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from student import Student
 import tutorial
+from flask import Flask
 
 def main():
-    student1 = Student("Leland", 9, "Arnav", "Vajirkar")
-    Student.add_student(student1)
 
+    student1 = Student("Leland", 9, "Arnav", "Vajirkar", 4.0)
+    Student.add_student(student1)
+    student1.add_classes(1, "Sarkar")
+    student1.add_classes(2, "Yen")
+
+    print(tutorial.get_tutorial(student1, "Friday"))
+
+    
+
+if __name__ == '__main__':
+    main()
