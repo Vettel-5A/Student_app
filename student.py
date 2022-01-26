84c631c10f36f70ac82a78249172012632c317a6
Eluyscuti <abhikaushikkar@gmail.com>
2022-01-26 10:48:26 -0800

    Add grade functionality

--- student.py
+++ student.py
@@ -10,12 +10,24 @@ class Student:
 
 
     def add_classes(self, period, teacher):
-        self.classes[period] = teacher
+        self.classes[period] = {teacher:None}
+
+    def add_grade(self, teacher,grade):
+        for value in self.classes.values():
+            for point in value.keys():
+                try:
+                    if point == teacher:
+                        value[point] = int(grade)
+
+                    else: 
+                        pass
+
+                except TypeError:
+                    pass
 
     @classmethod
     def add_student(cls, student):
         cls.students.append(student)
-
 
 
 
